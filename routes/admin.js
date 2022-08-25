03d6186b5155a32c19d4c9b0e49fd9d9feb1faa9
muhammed sheheem <shaimonsheheem@gmail.com>
2022-08-25 16:30:45 +0530

    new layout set, new navbar(model) set, differentiated for admin and user

--- routes/admin.js
+++ routes/admin.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index');
+  res.render('admin/admin-home',{admin:true});
 });
 
 module.exports = router;
